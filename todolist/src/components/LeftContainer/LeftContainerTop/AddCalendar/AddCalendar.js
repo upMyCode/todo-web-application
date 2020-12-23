aa93a05aea9cde6164e9f3620f406fad9f12a387
Ulad Birukou <alorine99@gmail.com>
2020-12-23 23:40:06 +0300

    add-switcher

--- todolist/src/components/LeftContainer/LeftContainerTop/AddCalendar/AddCalendar.js
+++ todolist/src/components/LeftContainer/LeftContainerTop/AddCalendar/AddCalendar.js
@@ -4,65 +4,64 @@ import './addCalendar.css';
 class AddCalendar extends Component {
     constructor(props){
         super(props);
-        this.elem = [];
-        this.date = props.date;
+        this.counter = 0;
+        this.elems = [];
         this.state = {
-            elems: this.listOfElems
-        };
-        this.getId = this.getId.bind(this);
-        this.addNumberOfDate = this.addNumberOfDate.bind(this);
+            elem: this.elems
+        }
+        this.date = props.date;  
     }
     
-    addNumberOfDate() {
+    addNumberOfDate = () => {
         if (this.date) {
             for (let i = 0; i < this.date; i++) {
-                this.elem.push({i});
-                
+                    this.elems.push({number: i, id: i, important: false});
+                } 
             }
         }
-    }
+    
 
-    getId(e){
-      let idElem = e.target;
-      if(idElem.id >= 0) {
-      this.setState(({elems}) => {
-          idElem.className = 'blue__elem';
-          this.listOfElems.push(idElem);
-          return {
-              elems : this.listOfElems
-          }
-        })
-      console.log(this.state.elems);
-    }
-    //   if(idElem === 'elem') {
-    //      idElem = 'blue__elem'
-    //      this.setState({cl: 'blue__elem'})
-    //   }
-    //   console.log(e.target.className);
+    getId = (e) => {
+      let idElem = e.target.id;
+      if(this.elems[idElem].important === false) { 
+          e.target.className = 'blue__elem';
+          this.elems[idElem].important = true;
+          this.setState({
+            elem: this.elems
+          })
+          console.log(this.state.elem)
+        }
+        else {
+            e.target.className = 'elem'
+            this.elems[idElem].important = false;
+            this.setState({
+            elem: this.elems
+          })
+        }
 
     }
-
-    render() {
-        this.addNumberOfDate();
-        this.listOfElems = this.elem.map((item,id) => 
-        <div key={id} className='elem' onClick={this.getId} >
-            <a className='elem__number'>{id + 1}</a>
+    render(){
+        this.addNumberOfDate()
+        this.listOfElems =  this.elems.map((item) => 
+        <div key={item.id} className='elem' important={item.important.toString()} id={item.id} onClick={this.getId} >
+            <a className='elem__number' id={item.id} >{item.number}</a>
         </div>
-         )
+        )
         
-
+    
         return (
             <div className='addCalendar'>
                 {   
-                    this.elem.map((item,id) => 
-                    <div key={id} className='elem' onClick={this.getId} >
-                        <a className='elem__number' id={id} >{id + 1}</a>
-                    </div>
+                    this.elems.map((item) => 
+                    <div key={item.id} className='elem' important={item.important.toString()}  id={item.id} onClick={this.getId} >
+                         <a className='elem__number' id={item.id} >{item.number}</a>
+                     </div>
                     )
-                }
+                } 
             </div>
         )
     }
 }
+
 
 export default AddCalendar;
