aed6c5bc61d0b41e372f1234b8078a6a5030b645
Ulad Birukou <alorine99@gmail.com>
2021-04-21 19:25:44 +0300

    add-importent-button

--- todoapp/src/UserInterface/ToDoTickets/component/ToDoTickets.js
+++ todoapp/src/UserInterface/ToDoTickets/component/ToDoTickets.js
@@ -1,18 +1,23 @@
-import React,{useContext, useEffect} from 'react'
+import React,{useContext, useEffect, useState} from 'react'
 import '../container/ToDoTickets.css'
 import {getMonthName} from '../../../DateInfo/Date/container/DateComponentLogic'
 import  pancel from '../container/pancel.png'
 import {Context} from '../../../App/container/context'
 
 
-export default function AddToDoTickets({userText,ticketsList, defaultDate, defaultMonth, minutes, hours, setTicketsList}) {
+export default function AddToDoTickets({userText, ticketsList, defaultDate, defaultMonth, minutes, hours, setTicketsList}) {
     
-        useEffect(()=>{
-            setTicketsList((prev)=> {
-                const a = prev;
-                a.splice(4, a.length -4);
-                return [...a]
-            })
+        const [ticketColor, setTicketColor] = useState(false);
+        const [idElem, setIdElem] = useState(0)
+
+        useEffect(() => {
+            if (ticketsList.length >= 5) {
+                setTicketsList((prev) => {
+                    const a = prev;
+                    a.splice(4, a.length - 4);
+                    return [...a]
+                });
+            }
         },[ticketsList])
 
         let month = getMonthName()
@@ -25,26 +30,38 @@ export default function AddToDoTickets({userText,ticketsList, defaultDate, defau
             })
         }
 
-        console.log(ticketsList)
-        
+        function addStatus(e) {
+
+            if (e.target.className == 'CheckPoint__unimportent') {
+                e.target.className = 'CheckPoint__importent';
+                setTicketColor(true);
+                setIdElem(e.target.id);
+                console.log(idElem);
+            } else {
+                e.target.className = 'CheckPoint__unimportent';
+                setTicketColor(false);
+            }
+        }
+
         return (
+
             <div className='container__ToDoTickets'>
                 
             {                
-                ticketsList.map((elem, id)=> {
+                ticketsList.map((elem, id) => {
                     return (
                         <div 
                              key={id} 
                              className='ToDoTickets__Ticket'>
-                            <div className='ToDoTickets__CheckPoint'>
-                                <div className='CheckPoint'>
-                            
-                                </div>
+                            <div className='ToDoTickets__CheckPoint'id={id}>
+                                <a className='CheckPoint__unimportent' onClick={addStatus} id={id}>
+
+                                </a>
                             </div>
-                            <div className='Ticket'>
+                            <div className={ticketColor == true ? 'Ticket__importent' : 'Ticket'} id={id}>
                                 <div className='Ticket__item-1'>
                                     <>
-                                        <p className='elem'>{elem}</p>
+                                        <p className={ticketColor == true  ? 'elem__importent' : 'elem'} id={id}>{elem}</p>
                                     </>
                                     <>
                                         <span className = 'defaultDate'>{`${defaultDate} ${month}    ${hours} : ${minutes}`}</span>
@@ -66,5 +83,6 @@ export default function AddToDoTickets({userText,ticketsList, defaultDate, defau
                 })
              }
             </div>
-        )  
+        )
+      
 }
